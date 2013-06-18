91d0c447d9593ce93b3d7d5bd83961ed052627b6
E.Azer Koçulu <azer@kodfabrik.com>
2013-06-17 23:37:32 -0700

    better modularity

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var entries     = require("./lib/entries"),
+    pageCount   = require('./lib/page-count'),
+    page        = require('./lib/page'),
     suggestions = require('./lib/suggestions');
 
-exports = module.exports = entries;
+exports             =
+module.exports      = entries;
+exports.page        = page;
+exports.pageCount   = pageCount;
 exports.suggestions = suggestions;
